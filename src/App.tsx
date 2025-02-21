7bbb64da1a5ddb8bd60a6727dccad8b1b8e10468
geist9110 <geist9110@naver.com>
2025-02-21 23:59:39 +0900

    feat: 사이드바 및 홈 클래스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
+import Sidebar from './components/Sidebar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app-container">
+      <BrowserRouter>
+        <Sidebar />
+        <div id="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
